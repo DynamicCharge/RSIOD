81329b5002e08ae42aecffe5217565a341d506b1
AlexanderMehighlove <armsof.theocean@mail.ru>
2021-11-02 11:41:33 +0300

    Слайдер оживлен и приведен в надлежащий вид.

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,3 +1,18 @@
 $(document).ready(function (){
-    alert('w');
+    $('.next').click(function(){
+        var currentSlide = $('.slider__item.current');
+        var currentSlideIndex = $('.slider__item.current').index();
+        var nextSlideIndex = currentSlideIndex + 1;
+        var nextSlide = $('.slider__item').eq(nextSlideIndex);
+        currentSlide.fadeOut(1);
+        currentSlide.removeClass('current');
+
+        if(nextSlideIndex == ($('.slider__item:last').index()+1)){
+            $('.slider__item').eq(0).fadeIn(500);
+            $('.slider__item').eq(0).addClass('current');
+        } else {
+            nextSlide.fadeIn(500);
+            nextSlide.addClass('current');
+        }
+    });
 });
